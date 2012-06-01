9f9f84ac5570ebfcd09b0273c2130b6e34f16453
moejoe <e0025125@student.tuwien.ac.at>
2012-06-01 22:32:00 +0200

    code cleanup

--- alogdat2/pr1/src/ads1ss12/pa/KMST.java
+++ alogdat2/pr1/src/ads1ss12/pa/KMST.java
@@ -15,14 +15,16 @@ import java.util.TreeSet;
 public class KMST extends AbstractKMST {
 	
 	public static final Integer INFINITE_WEIGHT = Integer.MAX_VALUE;
-	TreeSet<Edge> graph; 
-	Integer numNodes;
-	Integer numEdges;
-	int k;
-	int counter;
-	int cuts=0;
-	//KruskalKMST krustal;
-	PrimKMST prim;
+	
+	int recursion = 0;
+	
+	private HashSet<Edge> tree;
+	private int[] connectedNodes;
+	private int k;
+	ArrayList<Edge> graph;
+	
+	int curWeight;
+	int upperBound;	
 	
 	/**
 	 * Der Konstruktor. Hier ist die richtige Stelle f&uuml;r die
@@ -38,16 +40,18 @@ public class KMST extends AbstractKMST {
 	 * 			Die Anzahl der Knoten, die Ihr MST haben soll
 	 */
 	public KMST(Integer numNodes, Integer numEdges, HashSet<Edge> edges, int k) {
-		this.graph = new TreeSet<Edge>(edges);
-		this.numEdges = numEdges;
-		this.numNodes = numNodes;
-		this.k = k;
 		
+		this.graph = new ArrayList<Edge>(edges);
+		java.util.Collections.sort(this.graph);
+		
+		this.tree = new HashSet<Edge>((int)(numNodes*0.75));
+		this.connectedNodes = new int[numNodes];
+		this.k = k;
+		// initialize connectedNode list
+		for( int i=0; i< numNodes; i++ ) { connectedNodes[i] = 0; };
+		// Heuristik for an upper Bound
 		HashSet<Edge> sol = getHSolution(numNodes,numEdges,this.graph,k);
-		setSolution(calculateWeight(sol),sol);
-		Main.printDebug("Initial Solution: " + getSolution().getUpperBound());
-		prim = new PrimKMST(numNodes, k, this);
-		//krustal = new KruskalKMST(numNodes,graph,k,this);
+		setSolution(calculateWeight(sol),sol);	
 	}
 
 	/**
@@ -61,47 +65,12 @@ public class KMST extends AbstractKMST {
 	 */
 	@Override
 	public void run() {
-		counter = 0;
-		Main.printDebug("Starting with " + this.numEdges + " Edges - searching for " + k + "-MST");
-			
-		//simpleBaB(this.graph, null, this.k);
-		//krustal.run(this.graph);
-		prim.run(this.graph);
-		Main.printDebug("Done after " + counter + " iterations");
-	}
-	
-	public void simpleBaB(TreeSet<Edge> canditates, Edge lastMustEdge,int k){
-		this.counter++;
-		/* calculate best case scenario for this list */
-		if( lowerBound(canditates,k) > getSolution().getUpperBound()) {
-			this.cuts++;
-			return;
-		}
+		recursion = 0;	
+		upperBound = getSolution().getUpperBound();
+		curWeight = 0;
+		_recAlgorithm( this.graph );
 		
-		/* There is still hope */
-		
-		
-		/* we have a possible solution */
-		if( canditates.size() == k - 1 || ( lastMustEdge != null && canditates.tailSet(lastMustEdge).size() == k - 1 )){
-			HashSet<Edge> solution = canditates.size() == k - 1 ? new HashSet<Edge>(canditates) : new HashSet<Edge>(canditates.tailSet(lastMustEdge));			
-			if( isATree(solution) ){
-				if( setSolution(calculateWeight(solution),solution) ) {
-					Main.printDebug("New Solution is better :)" + getSolution().getUpperBound());
-					Main.printDebug(solution);
-				}
-			}
-			return;
-		}
-	
-		// remove most expensive edge and branch
-	
-		Edge removedEdge = lastMustEdge == null ? canditates.last() : canditates.lower(lastMustEdge);
-		canditates.remove(removedEdge);
-		simpleBaB(canditates,lastMustEdge,k);
-			
-		// force inclusion of the removed edge and start the other branch
-		canditates.add(removedEdge);
-		simpleBaB(canditates,removedEdge,k);
+		Main.printDebug("Done after " + recursion + " iterations");
 	}
 	
 	/**
@@ -111,7 +80,7 @@ public class KMST extends AbstractKMST {
 	 * - random ST
 	 * 
 	 */
-	public static HashSet<Edge> getHSolution(Integer numNodes, Integer numEdges, TreeSet<Edge> edges, int k){
+	public static HashSet<Edge> getHSolution(Integer numNodes, Integer numEdges, ArrayList<Edge> edges, int k){
 		HashSet<Edge> sol = new HashSet<Edge>( (int) (k * 1.25) );
 	
 		TreeSet<Edge> edgeClone = new TreeSet<Edge>(edges);
@@ -127,6 +96,76 @@ public class KMST extends AbstractKMST {
 		}
 		return sol;
 	}
+	
+
+	
+	private void _recAlgorithm( ArrayList<Edge> canditates ){
+		recursion++;
+		int spaceLeft = k - ( 1 + tree.size() ); // Wieviel Kanten haben noch Platz
+		
+		/* Neue Lösung gefunden */
+		if( tree.size() == this.k-1 ){
+			HashSet<Edge> solution = new HashSet<Edge>(tree);
+			if( setSolution(calculateWeight(solution),solution) ) {
+//				Main.printDebug("New Solution is better :)" + getSolution().getUpperBound());
+//				Main.printDebug(tree);
+			}
+			upperBound = getSolution().getUpperBound();
+			return;
+		}
+		
+		for( int i = 0; i < canditates.size(); i++ ){
+			Edge curCanditate = canditates.get(i);
+			
+			int bestCase = curWeight;
+			if( i >= spaceLeft ){
+				bestCase += curCanditate.weight;
+				for( int j = 0; j < spaceLeft - 1; j++ ) { bestCase += canditates.get(j).weight; };				
+			}
+			else {
+				for( int j = 0; j < spaceLeft; j++ ) { bestCase += canditates.get(j).weight; };
+			}
+			if( bestCase >= upperBound ) return;
+			
+			if( tree.isEmpty() || ( connectedNodes[curCanditate.node1] > 0 && connectedNodes[curCanditate.node2] == 0 )
+					|| ( connectedNodes[curCanditate.node1] == 0 && connectedNodes[curCanditate.node2] > 0 ) ) {
+			
+				/* Wir haben einen passenden mit min. Knoten gefunden */
+				connectedNodes[curCanditate.node1]++;
+				connectedNodes[curCanditate.node2]++;
+				
+				tree.add(curCanditate);
+				curWeight += curCanditate.weight;
+				
+				/* Der neue Kanditat wird danach nicht mehr in der Liste sein
+				 * und daher muss der Index angepasst werden.
+				 */
+				canditates.remove(i--);
+				
+				ArrayList<Edge> clone = new ArrayList<Edge>(canditates);
+				_recAlgorithm(clone);
+				
+				tree.remove(curCanditate);
+				
+				curWeight -= curCanditate.weight;
+				connectedNodes[curCanditate.node1]--;
+				connectedNodes[curCanditate.node2]--;
+
+				if( canditates.size() < spaceLeft ) return;
+				
+				bestCase = curWeight;
+				for( int j = 0; j < spaceLeft; j++ ) { bestCase += canditates.get(j).weight; };
+				
+				if( bestCase >= upperBound ) return;
+				
+				_recAlgorithm(clone);
+			}
+		}
+		return;
+	}
+	
+
+	
 
 	/**
 	 *  Gibt f&uuml;r eine Kante, und einen Graphen die Anzahl der 
@@ -207,201 +246,7 @@ public class KMST extends AbstractKMST {
 		return true;
 	}
 
-	private static class PrimKMST {
-		private HashSet<Edge> tree;
-		private int[] connectedNodes;
-		private KMST container; 
-		private int k;
-		//int bestCaseWeight;
-		int curWeight;
-		int upperBound;
-		
-		public PrimKMST(int numNodes, int k, KMST container ){
-			this.tree = new HashSet<Edge>((int)(numNodes*0.75));
-			this.connectedNodes = new int[numNodes];
-			this.k = k;
-			this.container = container;
-			for( int i=0; i< numNodes; i++ ) { connectedNodes[i] = 0; };
-		}
-		
-		public void run( TreeSet<Edge> edges ){
-			ArrayList<Edge> list = new ArrayList<Edge>(edges);
 
-			curWeight = 0;
-			upperBound = container.getSolution().getUpperBound();
-			_recAlgorithm( list );
-		}
 		
-		private void _recAlgorithm( ArrayList<Edge> canditates ){
-			int spaceLeft = k - ( 1 + tree.size() ); // Wieviel Kanten haben noch Platz
-			/* Neue Lösung gefunden */
-			if( tree.size() == this.k-1 ){
-				HashSet<Edge> solution = new HashSet<Edge>(tree);
-				if( container.setSolution(calculateWeight(solution),solution) ) {
-					Main.printDebug("New Solution is better :)" + container.getSolution().getUpperBound());
-					Main.printDebug(tree);
-				}
-				upperBound = container.getSolution().getUpperBound();
-				return;
-			}
-			
-			for( int i = 0; i < canditates.size(); i++ ){
-				Edge curCanditate = canditates.get(i);
-				
-				int bestCase = curWeight;
-				if( i >= spaceLeft ){
-					bestCase += curCanditate.weight;
-					for( int j = 0; j < spaceLeft - 1; j++ ) { bestCase += canditates.get(j).weight; };				
-				}
-				else {
-					for( int j = 0; j < spaceLeft; j++ ) { bestCase += canditates.get(j).weight; };
-				}
-				if( bestCase >= upperBound ) return;
-				
-				if( tree.isEmpty() || ( connectedNodes[curCanditate.node1] > 0 && connectedNodes[curCanditate.node2] == 0 )
-						|| ( connectedNodes[curCanditate.node1] == 0 && connectedNodes[curCanditate.node2] > 0 ) ) {
-				
-					/* Wir haben einen passenden mit min. Knoten gefunden */
-					connectedNodes[curCanditate.node1]++;
-					connectedNodes[curCanditate.node2]++;
-					
-					tree.add(curCanditate);
-					curWeight += curCanditate.weight;
-					
-					/* Der neue Kanditat wird danach nicht mehr in der Liste sein
-					 * und daher muss der Index angepasst werden.
-					 */
-					canditates.remove(i--);
-					
-					ArrayList<Edge> clone = new ArrayList<Edge>(canditates);
-					_recAlgorithm(clone);
-					
-					tree.remove(curCanditate);
-					
-					curWeight -= curCanditate.weight;
-					connectedNodes[curCanditate.node1]--;
-					connectedNodes[curCanditate.node2]--;
 
-					if( canditates.size() < spaceLeft ) return;
-					
-					_recAlgorithm(clone);
-				}
-				
-								
-				
-			}
-			return;
-		}
-		
-	}
-	
-	/* does not work as expected -.- should have thought it through */
-	/*
-	private static class KruskalKMST {
-		
-		private ArrayList<DynamicDisjunctSet> dds;
-		private HashSet<Edge> tree;
-		private int currentWeight = 0;
-		private int k;
-		private TreeSet<Edge> edges;
-		private KMST container = null;
-		
-		
-		private void addSet(DynamicDisjunctSet aSet){
-			dds.add(aSet.node, aSet);
-		}
-		public DynamicDisjunctSet getSet(int aNode){
-			//if(dds == null ) return null;
-			return dds.get(aNode);
-		}
-		public void divide(Edge anEdge){		
-			DynamicDisjunctSet set1 = getSet(anEdge.node1);
-			DynamicDisjunctSet set2 = getSet(anEdge.node2);
-
-			if(set1.parent == set2) {
-				set1.parent = set1;
-			}
-			if(set2.parent == set1){
-				set2.parent = set2;
-			}
-		}
-		
-		public KruskalKMST(Integer numNodes, TreeSet<Edge> edges, int k, KMST container) {
-			dds = new ArrayList<DynamicDisjunctSet>(numNodes);
-			for(int i = 0; i < numNodes; i++ ){
-				addSet(new DynamicDisjunctSet(i)); // TODO: Inline function
-			}
-			tree = new HashSet<Edge>((int)(numNodes/0.75));
-			this.edges = edges;
-			this.k = k;
-			this.container = container;
-			
-			 
-		}
-		
-		public void run(TreeSet<Edge> edges) {
-			// Wir sind fertig 
-			if( tree.size() == k-1 ){
-				if( container.setSolution(calculateWeight(tree),tree) ) {
-					Main.printDebug("New Solution is better :)" + container.getSolution().getUpperBound());
-					Main.printDebug(tree);
-				}
-			}
-			// Find next viable edge with lowest cost 			
-			for( Iterator<Edge> it = edges.iterator(); it.hasNext();  ){
-				Edge e = it.next();
-				DynamicDisjunctSet set1 = DynamicDisjunctSet.findRoot(getSet(e.node1));
-				DynamicDisjunctSet set2 = DynamicDisjunctSet.findRoot(getSet(e.node2));
-				
-				if(  set1 != set2 ){
-					set1.union(set2);
-					tree.add(e);
-					currentWeight += e.weight;
-					it.remove();
-					// Besser werden wir hier nicht mehr
-					if( currentWeight + lowerBound(edges,k - tree.size()) > container.getSolution().getUpperBound()) {
-						return;
-					}				
-					this.run((TreeSet)edges.clone());
-					
-					
-					tree.remove(e);
-					currentWeight -= e.weight;
-					divide(e);
-					// Besser werden wir hier nicht mehr
-					if( currentWeight + lowerBound(edges,k - tree.size()) > container.getSolution().getUpperBound()) {
-						return;
-					}
-					this.run((TreeSet)edges.clone());
-				}
-			}
-			// Es gibt keine gültigen Lösungen mehr 
-			return;
-		}
-		
-		private static class DynamicDisjunctSet{			
-			private int node;
-			public DynamicDisjunctSet parent;
-			
-			public DynamicDisjunctSet(int node){
-				this.node = node;
-				this.parent = this;
-			}
-			
-			public void union( DynamicDisjunctSet other ){
-				this.parent = other;
-			}
-			
-			public static DynamicDisjunctSet findRoot(DynamicDisjunctSet leaf){
-				DynamicDisjunctSet lastParent = leaf;
-				
-				while( lastParent != leaf ){
-					leaf = leaf.parent;
-				}
-				return leaf;
-			}
-		}
-		
-	}
-	*/
 }
